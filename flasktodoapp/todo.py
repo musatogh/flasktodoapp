137daf8ec353511be8939f4aa611aa03e567e2bf
musatogh <63783464+musatogh@users.noreply.github.com>
2023-04-12 21:54:55 +0300

    Add files via upload

---
+++ flasktodoapp/todo.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/toghrulmusayevv/Desktop/ToDoApp/todo.db"
+db = SQLAlchemy(app)
+@app.route("/")
+def index():
+    
+    todos = Todo.query.all()
+    
+    return render_template("index.html", todos = todos)
+
+#AddTodo
+@app.route("/add", methods = ["POST"])
+def addTodo():
+    title = request.form.get("title")
+    newTodo = Todo(title = title, complete = False)
+    
+    db.session.add(newTodo)
+    db.session.commit()
+    
+    return redirect(url_for("index"))
+
+#CompleteTodo
+@app.route("/complete/<string:id>")
+def completeTodo(id):
+    todo = Todo.query.filter_by(id = id).first()
+    
+    # if todo.complete == True:
+    #     todo.complete = False
+    # else:
+    #     todo.complete = True        
+    
+    todo.complete = not todo.complete
+    
+    db.session.commit()
+    return redirect(url_for("index"))
+
+#RemoveTodo
+@app.route("/delete/<string:id>")
+def deleteTodo(id):
+    todo = Todo.query.filter_by(id = id).first()
+    
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+#ClassTodo
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+    
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
